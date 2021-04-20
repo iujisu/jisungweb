9d7adc567c3d8ae63e339ebbfdcf449b67ad8bbb
jisungyou <iujisu@naver.com>
2021-04-20 19:09:10 +0900

    test

---
+++ src/main/java/com/jisungweb/relationships/JisungwebApplication.java
@@ -0,0 +1,13 @@
+package com.jisungweb.relationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JisungwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JisungwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jisungweb/relationships/JisungwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jisungweb.relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JisungwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
