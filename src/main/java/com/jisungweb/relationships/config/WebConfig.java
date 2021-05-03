686c93fc3e61e07fbb092c1892d6c7c5db45396d
jisungyou <iujisu@naver.com>
2021-05-03 20:42:45 +0900

    file upload test

--- src/main/java/com/jisungweb/relationships/config/WebConfig.java
+++ src/main/java/com/jisungweb/relationships/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.jisungweb.relationships.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,4 +15,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST")
                 .maxAge(3000);
     }
+    
+    @Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("UTF-8"); // 파일 인코딩 설정
+		multipartResolver.setMaxUploadSizePerFile(5 * 1024 * 1024); // 파일당 업로드 크기 제한 (5MB)
+		multipartResolver.setMaxUploadSize(5 * 1024 * 1024);
+		return multipartResolver;
+	}
+
 }
--- src/main/java/com/jisungweb/relationships/member/controller/MemberController.java
+++ src/main/java/com/jisungweb/relationships/member/controller/MemberController.java
@@ -4,38 +4,51 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.RequestEntity;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jisungweb.relationships.member.service.MemberService;
 import com.jisungweb.relationships.member.vo.MemberVo;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @RestController
 @RequestMapping("/api")
 @Slf4j
 public class MemberController {
+	
+	public static final Logger logger = LoggerFactory.getLogger(MemberController.class); 
 
 		@Autowired
 		private   MemberService memService;
+		
 		@GetMapping("/hello")
-		@ApiOperation(value="사용자 정보 등록",notes = "사용자 정보 등록을 한다")
 		public String hello() {
+			log.info("정보성 로그");
 			return "<h1>member/hello spring boot</h1>";
 		}
 
-		@PostMapping("/memberadd")
-		public ResponseEntity<Map<String, String>> memberAdd(@RequestBody MemberVo memberVo) {
+		@PostMapping(value="/memberadd", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
+		@ApiOperation(value="사용자 정보 등록",notes = "사용자 정보 등록을 한다")
+		public ResponseEntity<?> memberAdd(@RequestBody MemberVo memberVo,@RequestPart("file") MultipartFile file) {
 			System.out.println("====MemberController.insertUser====");
 			System.out.println("getUserId==>>>"+memberVo.getUserId());
+			System.out.println("getUserId==>>>"+memberVo.getPhoneNumber());
+			
+			logger.info("file = " + file.getSize());
+		
+			
 			memService.insertUser(memberVo);
 			
 			Map<String, String> map = new HashMap<String, String>();
